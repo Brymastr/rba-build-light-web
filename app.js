8b57f06c740222544fa66c5a010ba1ed897aec43
Brycen <dorsay@live.ca>
2016-06-15 23:46:17 -0700

    Initial commit

---
+++ app.js
@@ -0,0 +1,8 @@
+var app = angular.module('app', ['ngRoute']);
+app.config(function($routeProvider, $locationProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'views/home.html',
+      controller: 'HomeController'
+    });
+});

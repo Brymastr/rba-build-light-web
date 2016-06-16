984f26c122b1bc7a1b702d45c84db33e770712c3
Brycen Dorsay <bdorsay@rbauction.com>
2016-06-16 13:33:14 -0700

    Create lights and fetch light list

--- controllers/lightsController.js
+++ controllers/lightsController.js
@@ -1,3 +1,35 @@
-app.controller('LightsController', function($scope) {
+app.controller('LightsController', function($scope, $http) {
   $scope.lights = "Lights controller";
+  const url = 'http://localhost:9001/api/lights'
+  
+  $scope.newLightName;
+  $scope.newLightIp;
+  $scope.lights = [];
+
+  $scope.createLight = function() {
+    var data = {
+      name: $scope.newLightName,
+      ip: $scope.newLightIp
+    }
+    $http.post(url, data, null).then(
+      function(response) {
+        console.log('new light created');
+      },
+      function(response) {
+        console.log('error');
+      }
+    );
+  }
+
+  $scope.fetchList = function() {
+    $http.get(url, null).then(
+      function(response) {
+        $scope.lights = response.data;
+        console.log(response.data);
+      },
+      function(response) {
+        console.log('error');
+      }
+    );
+  }
 });

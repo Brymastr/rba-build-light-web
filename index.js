0be9774c46c1b10cf66891fee7cfb657ac7a28e8
Brycen <dorsay@live.ca>
2016-06-17 11:45:13 -0700

    Change port env var name

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ app.all('/*', function(req, res) {
   res.sendfile('index.html');
 });
 
-var port = process.env.USB_BUILD_LIGHT_WEB_PORT || 9000;
+var port = process.env.BUILD_LIGHT_WEB_PORT || 9000;
 http.createServer(app).listen(port, function() {
   console.log("server listening on port " + port);
 });

35e7f06e683619e91d8dab7259448708609cfa08
Brycen Dorsay <bdorsay@rbauction.com>
2016-06-27 16:50:46 -0700

    Gulp for config with environment variables

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,25 @@
 var gulp = require('gulp');
-var apiURL = process.env.BUILD_LIGHT_PROXY_API_URL || 'http://10.2.69.35';
-var apiPort = process.env.BUILD_LIGHT_PROXY_API_PORT || 9002;
+var plugins = require('gulp-load-plugins')();
+
+var apiUrl = process.env.BUILD_LIGHT_PROXY_API_URL || 'http://10.2.69.35';
+var apiPort = process.env.BUILD_LIGHT_PROXY_API_PORT || 9001;
+
+gulp.task('config', function() {
+  gulp.src(['config.template.js'])
+    .pipe(plugins.replace(buildRegex('apiUrl'), replaceVariable('apiUrl', apiUrl)))
+    .pipe(plugins.replace(buildRegex('apiPort'), replaceVariable('apiPort', apiPort)))
+    .pipe(plugins.rename('config.js'))
+    .pipe(gulp.dest('./'));
+});
+
+function replaceVariable(variable, value) {
+  return '\'' + variable + '\': \'' + value + '\'';
+}
+
+function buildRegex(variable) {
+  var x = new RegExp("['\"](" + variable + ")['\"]:[ a-zA-Z0-9'\"\/.:]*");
+  console.log(x)
+  return x;
+}
+
+gulp.task('default', ['config']);
